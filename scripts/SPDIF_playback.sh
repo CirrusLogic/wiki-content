e91092d58b0c5cdc5ab4dd61243c2cab6eb2d55a
Francis Bain <fb@opensource.wolfsonmicro.com>
2015-03-26 07:26:55 +0000

    rpi-scripts: Updated scripts for the rpi-3.18.y driver

--- scripts/SPDIF_playback.sh
+++ scripts/SPDIF_playback.sh
@@ -3,7 +3,7 @@
 # $1 added to support 1st line argument. i.e. "./SPDIF_playback.sh -q" will stop all the control information being displayed on screen
 
 #Playback to SPDIF Out Enable
-amixer $1 -Dhw:sndrpiwsp cset name='SPDIF out Switch' on
+amixer $1 -Dhw:sndrpiwsp cset name='SPDIF Out Switch' on
 # Setup WM5102 AIF2 stereo output source from AIF1 (data from AP), 0dB Gain
 amixer $1 -Dhw:sndrpiwsp cset name='Tx Source' AIF
 amixer $1 -Dhw:sndrpiwsp cset name='AIF2TX1 Input 1' AIF1RX1
--- scripts/SPDIF_record.sh
+++ scripts/SPDIF_record.sh
@@ -3,7 +3,7 @@
 # $1 added to support 1st line argument. i.e. "./SPDIF_record.sh -q" will stop all the control information being displayed on screen
 
 #Record from SPDIF in
-amixer $1 -Dhw:sndrpiwsp cset name='SPDIF in Switch' on
+amixer $1 -Dhw:sndrpiwsp cset name='SPDIF In Switch' on
 # Setup WM5102 AIF1 stereo output source from AIF2 (data from WM8804), 0dB Gain
 amixer $1 -Dhw:sndrpiwsp cset name='AIF1TX1 Input 1' AIF2RX1
 amixer $1 -Dhw:sndrpiwsp cset name='AIF1TX1 Input 1 Volume' 32
